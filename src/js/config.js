f537ed8a710b54c82c7fcf8eb486d82bd9af7db2
kwiatkwsk <kwiatkowska.kld@icloud.com>
2023-11-12 16:16:19 +0100

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'e1d174b7-5a3f-4ebc-b11a-48f3027cbbd6';
+export const MODAL_CLOSE_SEC = 2.5;
